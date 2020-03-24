f32e77cf155fb4665131942291e99e96924e2494
Hydroxide <62195284+Hydroxide001@users.noreply.github.com>
2020-03-24 17:24:14 -0600

    Create launcher.lua

---
+++ launcher.lua
@@ -0,0 +1,27 @@
+local whitelist = {}
+local version = 1.2
+
+AutoUpdate = true
+Premium = false
+BetaRelease = false
+
+loadstring(game:HttpGetAsync("https://pastebin.com/raw/Z5ykhhdt"))()
+loadstring(game:HttpGetAsync("https://pastebin.com/raw/v7Aai7m8"))()
+
+function load(file)
+    if type(file) == "number" then
+        return game:GetObjects("rbxassetid://" .. file)()
+    else
+        return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Hydroxide001/Hydroxide/master/" .. file))() -- github url
+    end
+    end
+    function Update() -- AutoUpdate Function
+    if HydroxideVersion == version then
+print("Already at latest version!")
+    else
+warn("Outdated version\n Updating...")
+load("main.lua")
+print("Done!")
+    end
+end
+load(Hydroxide.lua)
