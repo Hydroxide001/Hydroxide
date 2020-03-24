46d82cbe1718fe08ced2847d2ba46f04dde63296
Hydroxide <62195284+Hydroxide001@users.noreply.github.com>
2020-03-24 17:46:11 -0600

    Update Hydroxide.lua

--- Hydroxide.lua
+++ Hydroxide.lua
@@ -1,5 +1,5 @@
 version = 1.21
-MSG = loadstring(game:HttpGetAsync("https://pastebin.com/raw/Z5ykhhdt"))
+loadstring(game:HttpGetAsync("https://pastebin.com/raw/Z5ykhhdt"))
 warn("Hydroxide has been loaded!")
 warn("You are currently on version " .. version .. " of Hydroxide")
 warn(MSG)
