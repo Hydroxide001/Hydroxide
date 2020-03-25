e73b565db4e699c261d0bc286a6911bb767b3bfd
Hydroxide <62195284+Hydroxide001@users.noreply.github.com>
2020-03-24 19:47:54 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,18 @@
+--[[
+888    888               888                          d8b      888          
+888    888               888                          Y8P      888          
+888    888               888                                   888          
+8888888888 888  888  .d88888 888d888 .d88b.  888  888 888  .d88888  .d88b.  
+888    888 888  888 d88" 888 888P"  d88""88b `Y8bd8P' 888 d88" 888 d8P  Y8b 
+888    888 888  888 888  888 888    888  888   X88K   888 888  888 88888888 
+888    888 Y88b 888 Y88b 888 888    Y88..88P .d8""8b. 888 Y88b 888 Y8b.     
+888    888  "Y88888  "Y88888 888     "Y88P"  888  888 888  "Y88888  "Y8888  
+                888                                                         
+           Y8b d88P                                                         
+            "Y88P"          
+]]
 -- load icons and stuff here
 
-warn("Hydroxide has been loaded!")
-warn("You are currently on version " .. version .. " of Hydroxide")
-warn(MSG)
+print("Hydroxide has been loaded!")
+print("You are currently on version " .. version .. " of Hydroxide")
+pr(MSG)
