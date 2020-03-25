ef3683bc34cadedaca98e3ed6d47cf5845cf6233
Hydroxide <62195284+Hydroxide001@users.noreply.github.com>
2020-03-24 23:36:50 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ local mouse = client:GetMouse()
 
 loadstring(game:HttpGetAsync("https://pastebin.com/raw/Z5ykhhdt"))()
 getgenv().Hydroxide = {}
-getgenv().load function(file) -- load function
+getgenv().load = function(file) -- load function
     if type(file) == "number" then
         return game:GetObjects("rbxassetid://" .. file)()
     else
